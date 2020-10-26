1b7d595593e9e0ec91146fb67724cd5770061c2f
zjschulz <zjschulz@gmail.com>
2020-10-26 18:18:21 +0000

    move recipe array to service, create getRecipe function

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,13 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
-    
+    private recipes: Recipe[] = [
+        new Recipe('Cupcake', 'Test Description', 'https://www.fotolip.com/wp-content/uploads/2016/05/Cupcakes-Clipart-5.png');
+        new Recipe('Cupcake', 'Test Description', 'https://www.fotolip.com/wp-content/uploads/2016/05/Cupcakes-Clipart-5.png')
+      ];
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
 }
