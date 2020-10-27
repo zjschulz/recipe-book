3cea15e49d5eff1e628cb7c5fef6ac0e73b6cc78
zjschulz <zjschulz@gmail.com>
2020-10-27 13:02:40 +0000

    update recipe detail to display recipe's ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,19 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Cupcake', 'Test Description', 'https://www.fotolip.com/wp-content/uploads/2016/05/Cupcakes-Clipart-5.png'),
-        new Recipe('Cupcake', 'Test Description', 'https://www.fotolip.com/wp-content/uploads/2016/05/Cupcakes-Clipart-5.png')
+        new Recipe(
+            'Cupcake',
+            'Test Description',
+            'https://www.fotolip.com/wp-content/uploads/2016/05/Cupcakes-Clipart-5.png',
+            [
+                new Ingredient('flour', 2),
+                new Ingredient('sugar', 3)
+            ])
       ];
 
     getRecipes() {
