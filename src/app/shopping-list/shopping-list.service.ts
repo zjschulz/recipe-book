48341fc8b111bdfb921c2f3fc35f72d21d587684
zjschulz <zjschulz@gmail.com>
2020-10-27 12:51:50 +0000

    correct service to handle updating list with new items

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,9 @@
 import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Oranges', 10)
@@ -12,5 +15,6 @@ export class ShoppingListService {
     
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
