4ca0803bc1b5c84b2285f45d1eec9fac8ccf1f15
zjschulz <zjschulz@gmail.com>
2020-11-03 16:20:51 +0000

    clean up old navigation feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-book';
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature
-  }
-
+  
 }
