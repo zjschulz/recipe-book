89de21704837c02737f5e175f54b5371ea6d7ee4
zjschulz <zjschulz@gmail.com>
2020-11-03 00:12:38 +0000

    update paths in routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full'},
     { path: 'recipes', component: RecipesComponent, children: [
-        { path: ''}
+        { path: '', component: RecipeStartComponent }
     ] },
     { path: 'shopping-list', component: ShoppingListComponent }
 ];
