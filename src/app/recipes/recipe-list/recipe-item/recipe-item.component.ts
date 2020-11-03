c04f99a972051463daa7f49244eb76bc273b62e0
zjschulz <zjschulz@gmail.com>
2020-11-03 15:50:51 +0000

    fix item onclick functionality with routing

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -8,8 +8,9 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Input() index: number;
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
