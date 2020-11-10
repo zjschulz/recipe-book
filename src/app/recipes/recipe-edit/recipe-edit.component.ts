3bd1c621c0fc3214655d2303b5ebf509c80eaaf5
zjschulz <zjschulz@gmail.com>
2020-11-10 16:12:30 +0000

    build add ingredient functionality

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -35,6 +35,15 @@ export class RecipeEditComponent implements OnInit {
     
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    )
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
