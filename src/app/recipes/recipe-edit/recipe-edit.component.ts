8e99d1c5828e70766b2804595fb0239a932f0b73
zjschulz <zjschulz@gmail.com>
2020-11-11 15:31:01 +0000

    add functionality to cancel button and add redirects

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
-import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -15,7 +14,8 @@ export class RecipeEditComponent implements OnInit {
   recipeForm: FormGroup;
 
   constructor(private route: ActivatedRoute,
-              private recipeService: RecipeService) { }
+              private recipeService: RecipeService,
+              private router: Router) { }
 
   get controls() {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
@@ -44,6 +44,7 @@ export class RecipeEditComponent implements OnInit {
     else {
       this.recipeService.addRecipe(this.recipeForm.value)
     }
+    this.onCancel();
   }
 
   onAddIngredient() {
@@ -56,6 +57,10 @@ export class RecipeEditComponent implements OnInit {
         ])
       })
     )
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], {relativeTo: this.route})
   }
 
   private initForm() {
