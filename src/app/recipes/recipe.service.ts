887e6fa36fe9debc341e4087c1885444a7a6260b
zjschulz <zjschulz@gmail.com>
2020-11-16 22:56:56 +0000

    add transform to response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -27,6 +27,8 @@ export class RecipeService {
                 ]), 
       ];
 
+    // private recipes: Recipe[] = [];
+
     constructor(private slService: ShoppingListService) {}
     
     setRecipes(recipes: Recipe[]) {
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -20,6 +21,11 @@ export class DataStorageService {
     fetchRecipes() {
         this.http
             .get<Recipe[]>('https://recipe-book-b0e4b.firebaseio.com/recipes.json')
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+                });
+            }))
             .subscribe(recipes => {
                 this.recipeService.setRecipes(recipes);
             })
