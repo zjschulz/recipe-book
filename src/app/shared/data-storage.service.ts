2f5a6b80d1731c3238258ccc03c54933f1a67a2e
zjschulz <zjschulz@gmail.com>
2020-11-16 22:34:22 +0000

    create put request for stored recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient,
+                private recipeService: RecipeService) {}
+
+    storedRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.http
+            .put('https://recipe-book-b0e4b.firebaseio.com/recipes.json', recipes)
+            .subscribe(response => {
+                console.log(response);
+            });
+
+    }
 }
