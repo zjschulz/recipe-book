d662bc8ef9e34aabe69fcc50d32be65679ab2de9
zjschulz <zjschulz@gmail.com>
2020-11-20 14:26:10 +0000

    addloading spinner for auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,6 +8,7 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent {
     isLoginMode = true;
+    isLoading = false;
 
     constructor(private authService: AuthService) {}
 
@@ -22,6 +23,7 @@ export class AuthComponent {
         const email = form.value.email;
         const password = form.value.password;
 
+        this.isLoading = true;
         if (this.isLoginMode) {
 
         }
@@ -29,9 +31,11 @@ export class AuthComponent {
             this.authService.signup(email, password).subscribe(
                 resData => {
                     console.log(resData);
+                    this.isLoading = false;
                 },
                 error => {
                     console.log(error);
+                    this.isLoading = false;
                 });
         }
 
