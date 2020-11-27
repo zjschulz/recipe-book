9267a1980e00d0ffca5f4dedd54a4bb46877cf0b
zjschulz <zjschulz@gmail.com>
2020-11-27 16:45:34 +0000

    add logout functionality

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
+import { Router } from '@angular/router';
 
 export interface AuthResponseData {
     kind: string;
@@ -18,7 +19,8 @@ export interface AuthResponseData {
 export class AuthService {
     user = new BehaviorSubject<User>(null);
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient,
+                private router: Router) {}
 
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
@@ -52,7 +54,12 @@ export class AuthService {
         );
     }
 
-    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    logout() {
+        this.user.next(null);
+        this.router.navigate(['/auth']);
+    }
+
+    private (email: string, userId: string, token: string, expiresIn: number) {
         const expirationDate = new Date(
             new Date().getTime() + expiresIn * 1000);
         const user = new User(
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -34,6 +34,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
         this.dataStorageService.fetchRecipes().subscribe();
     }
 
+    onLogout() {
+        this.authService.logout();
+    }
+
     ngOnDestroy() {
         this.userSub.unsubscribe();
     }
