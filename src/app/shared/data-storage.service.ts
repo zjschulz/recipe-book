d13cb315e37efa1f73a0021f1adf92c2441cd4f0
zjschulz <zjschulz@gmail.com>
2020-11-27 16:36:33 +0000

    implement interceptor for tokens

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -21,16 +21,10 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(
-            take(1),
-            exhaustMap(user => {
-                return this.http.get<Recipe[]>(
-                    'https://recipe-book-b0e4b.firebaseio.com/recipes.json',
-                    {
-                        params: new HttpParams().set('auth', user.token)
-                    }
-                );
-            }),
+        return this.http.get<Recipe[]>(
+            'https://recipe-book-b0e4b.firebaseio.com/recipes.json'
+        )
+        .pipe(
             map(recipes => {
                 return recipes.map(recipe => {
                     return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
