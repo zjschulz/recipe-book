ee22612c6ea3316980385579deed9bd6886812ba
zjschulz <zjschulz@gmail.com>
2020-11-29 23:49:40 +0000

    update canActivate to account for edge cases

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,7 +2,7 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Rout
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
@@ -14,7 +14,9 @@ export class AuthGuard implements CanActivate {
         route: ActivatedRouteSnapshot,
         router: RouterStateSnapshot
     ): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
-        return this.authService.user.pipe(map(user => {
+        return this.authService.user.pipe(
+            take(1),
+            map(user => {
             const isAuth = !!user
             if (isAuth) {
                 return true;
