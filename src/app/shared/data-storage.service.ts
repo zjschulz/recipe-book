26c759f5bbb0786beccd0e49608f3160b38f7bd3
zjschulz <zjschulz@gmail.com>
2020-11-30 14:23:00 +0000

    remove unused items

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,14 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map, tap, take, exhaustMap } from 'rxjs/operators';
-import { AuthService } from '../auth/auth.service';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
     constructor(private http: HttpClient,
-                private recipeService: RecipeService,
-                private authService: AuthService) {}
+                private recipeService: RecipeService) {}
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
