fc6b27cdfa5e3dae8f928d8d4f019c70128bf5ff
zjschulz <zjschulz@gmail.com>
2020-12-04 18:57:23 +0000

    add imports to module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,6 +5,9 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [
@@ -14,6 +17,12 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
         RecipeItemComponent,
         RecipeStartComponent,
         RecipeEditComponent
+    ],
+    imports: [
+        RouterModule,
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule
     ],
     exports: [
         RecipesComponent,
