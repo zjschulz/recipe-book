3496bb443b726dca80bf30e98bad9a70d493b7c6
zjschulz <zjschulz@gmail.com>
2020-12-07 16:38:46 +0000

    implement lazy loading for auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const appRoutes: Routes = [
         path: 'shopping-list',
         loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)
     },
+    { 
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    }
 ];
 
 @NgModule({
